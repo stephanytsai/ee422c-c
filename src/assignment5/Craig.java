09282e5b15a398167c5b64b9a36def19c24bc7e2
stephanytsai <Stephany@wireless-10-145-205-78.public.utexas.edu>
2016-11-09 14:43:39 -0600

    runstats string

--- src/assignment5/Craig.java
+++ src/assignment5/Craig.java
@@ -50,11 +50,12 @@ public class Craig extends Critter {
 		dir = (dir + turn) % 8;
 	}
 
-	public static void runStats(java.util.List<Critter> craigs) {
+	public static String runStats(java.util.List<Critter> craigs) {
 		int total_straight = 0;
 		int total_left = 0;
 		int total_right = 0;
 		int total_back = 0;
+		String ret; 
 		for (Object obj : craigs) {
 			Craig c = (Craig) obj;
 			total_straight += c.genes[0];
@@ -62,12 +63,11 @@ public class Craig extends Critter {
 			total_back += c.genes[4];
 			total_left += c.genes[5] + c.genes[6] + c.genes[7];
 		}
-		System.out.print("" + craigs.size() + " total Craigs    ");
-		System.out.print("" + total_straight / (GENE_TOTAL * 0.01 * craigs.size()) + "% straight   ");
-		System.out.print("" + total_back / (GENE_TOTAL * 0.01 * craigs.size()) + "% back   ");
-		System.out.print("" + total_right / (GENE_TOTAL * 0.01 * craigs.size()) + "% right   ");
-		System.out.print("" + total_left / (GENE_TOTAL * 0.01 * craigs.size()) + "% left   ");
-		System.out.println();
+		ret="" + craigs.size() + " total Craigs    " + total_straight / (GENE_TOTAL * 0.01 * craigs.size()) + 
+				"% straight   " + total_back / (GENE_TOTAL * 0.01 * craigs.size()) + "% back   "
+				+ total_right / (GENE_TOTAL * 0.01 * craigs.size()) + "% right   "
+				+ total_left / (GENE_TOTAL * 0.01 * craigs.size()) + "% left   ";
+		return ret;
 	}
 	
 	@Override
